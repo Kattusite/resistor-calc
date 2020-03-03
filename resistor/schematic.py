b0014f9057a7077083afd90532787b87d3f24333
Kattusite <Kattusite@users.noreply.github.com>
2020-03-03 02:13:09 -0500

    schematic pseudocode

--- resistor/schematic.py
+++ resistor/schematic.py
@@ -17,3 +17,55 @@
 #
 # We will likely need to treat schematics as 2D floating buffers of characters, and
 # keep track of their bounding boxes and terminals
+
+# no need to track terminals - just track width/height, and assume terminals are always at midpoints
+
+
+class Schematic:
+
+    # For operations, we need:
+    # 1) create schematic from primitive resistor
+    # 2) connect schematics in series
+    # 3) connect schematics in parallel
+
+    # Also need some general helpers for 2d char arrays
+
+    def __init__(self, resistor):
+
+        op = "" if not resistor.history else resistor.history["operation"]
+
+        # base case
+        s = ""
+        if not resistor.history:
+            s = f"-({resistor.shortOhms()}Ω)-"
+        elif op == PARALLEL:
+            # recursively fetch schematics of children
+
+            # figure out max width & total height, allocate new buffer
+
+            # allocate each child its requested height, plus 1 padding between
+
+            # place children into buffer one at a time, centering them in a row
+
+        else # op == SERIES:
+            # recursively fetch schematics of children
+
+            # figure out max height & total width, allocate new buffer
+
+            # allocate each child its requested width, plus 1 padding between
+
+            # place children into buffer one at a time, centering them vertically
+
+
+
+
+
+
+        self.width = w
+        self.height = h
+
+        self.text = [
+            [x, y, z],
+            [a, b, c],
+            [d, e, f]
+        ] # W x H
